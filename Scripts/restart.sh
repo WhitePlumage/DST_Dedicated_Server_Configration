7f575636df0517801dc20b0847842ee33c260c35
WhitePlumage <32635321+WhitePlumage@users.noreply.github.com>
2019-06-29 19:04:19 +0800

    Update restart.sh

--- Scripts/restart.sh
+++ Scripts/restart.sh
@@ -8,6 +8,6 @@ name_folder="/home/steamuser/dst/bin"
 start_overworld="sudo sh start.sh"
 
 #Start or Restart the server
-screen -dr dst_server1 -X -S quit
+screen -dr dst1 -X -S quit
 cd ${name_folder}
 screen -S dst1 ${start_overworld}
